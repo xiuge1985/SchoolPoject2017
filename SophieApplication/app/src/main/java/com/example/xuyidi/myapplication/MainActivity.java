e2dcaa3a7fb473370c922836b361e0586482cfbe
SophieXu6666 <holso6666@gmail.com>
2017-02-18 17:57:10 -0500

    Sophie

---
+++ SophieApplication/app/src/main/java/com/example/xuyidi/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.xuyidi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setLogo(R.drawable.ic_launcher);
+        toolbar.setTitle("Home");
+        //toolbar.setSubtitle("Sub title");
+        setSupportActionBar(toolbar);
+        //toolbar.setNavigationIcon(R.drawable.ic_launcher);
+        toolbar.setOnMenuItemClickListener(onMenuItemClick);
+    }
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            String msg = "";
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    break;
+            }
+            if (!msg.equals("")) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 為了讓 Toolbar 的 Menu 有作用，這邊的程式不可以拿掉
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
